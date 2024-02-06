d73bab8aac4db30dc4e686523be30a04e6f1743c
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-02-06 16:49:31 +0300

    Added mobile header

--- src/modules/header/components/header-mobile.tsx
+++ src/modules/header/components/header-mobile.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import { HeaderProps } from '../interfaces/header.interfaces'
+import React from 'react';
+import { HeaderProps } from '../interfaces/header.interfaces';
+import Image from 'next/image';
+import viktor from '@/assets/textlogo.png';
 
 export const HeaderMobile = (props: HeaderProps) => {
   return (
-    <div className='block md:hidden'>HeaderMobile</div>
-  )
-}
+    <div className='block md:hidden'>
+      <div className='flex items-center p-5'>
+        <a href='/'>
+          <Image src={viktor} alt='Viktor' width={120} height={200} />
+        </a>
+      </div>
+    </div>
+  );
+};
