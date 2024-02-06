3b5b86d996a7107e45d9c99f80fa00ccdbf045e6
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-02-06 15:08:04 +0300

    Made a footer

--- src/modules/footer/footer.tsx
+++ src/modules/footer/footer.tsx
@@ -1,19 +1,33 @@
-'use client'
+'use client';
 
-import { Layout } from '@/components/ui/layout'
-import React from 'react'
+import { Layout } from '@/components/ui/layout';
+
+import React from 'react';
 
 export const Footer = () => {
   return (
-    <footer className='flex justify-between w-full h-32 bg-customBackground rounded-t-3xl'>
+    <footer className='flex justify-between w-full py-20 bg-black'>
       <Layout>
-        <p>
-          tg:
-          <a>
-
-          </a>
-        </p>
+        <div className='grid grid-cols-2'>
+          <div>
+            {/* <img src='/footer.png' alt='' /> */}
+          </div>
+          <div className='flex gap-y-5 flex-col'>
+            <div className='flex flex-row justify-center items-center gap-5'>
+              <p className='text-3xl text-white w-fit'>tg:</p>
+              <div className='bg-[#CDFF8D] p-4 rounded-2xl text-3xl'>
+                shampiniony
+              </div>
+            </div>
+            <div className='flex flex-row justify-center items-center gap-5'>
+              <p className='text-3xl text-white w-fit'>contact:</p>
+              <div className='bg-[#CDFF8D] p-4 rounded-2xl text-3xl'>
+                officialsh
+              </div>
+            </div>
+          </div>
+        </div>
       </Layout>
     </footer>
-  )
-}
+  );
+};
--- src/modules/mail-sender/mail-sender.tsx
+++ src/modules/mail-sender/mail-sender.tsx
@@ -1,34 +1,28 @@
-import { Border } from '@/components/ui/border'
+import { Border } from '@/components/ui/border';
 import { Button } from '@/components/ui/button';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export const MailSender = () => {
   const [main, setMain] = useState('');
 
-  const sendEmail = () => {
-
-  }
+  const sendEmail = () => {};
 
   return (
     <section>
       <h3 className='text-3xl text-center text-white'>
-        Получите ссылку на тестирование<br/>
-        бета-версии приложения
+        Запись на лист ожидания <br /> на закрытое бета-тестирование.
       </h3>
 
-      <Border
-        className='flex items-center gap-x-12 w-max mx-auto'
-      >
+      <Border className='flex items-center gap-x-12 w-max mx-auto'>
         <input
           className='px-4 py-2 rounded-full w-96'
           placeholder='Введите свой email'
         />
 
-        <Button
-          className='text-lg px-8'
-          onClick={sendEmail}
-        >Отправить</Button>
+        <Button className='text-lg px-8' onClick={sendEmail}>
+          Отправить
+        </Button>
       </Border>
     </section>
-  )
-}
+  );
+};
