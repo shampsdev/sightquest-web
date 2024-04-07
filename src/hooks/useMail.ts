6b2828ed89f2bc3040f71d4525196b691d2f69fa
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-04-08 01:26:55 +0300

    Added useEmail hook

---
+++ src/hooks/useMail.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const useEmail = () => {
+  const sendEmail = (email: string) => {
+    const options = {
+      method: 'POST',
+      url: 'https://sightquest.ru/api/subscribers/',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: { email },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  return {
+    sendEmail,
+  };
+};
