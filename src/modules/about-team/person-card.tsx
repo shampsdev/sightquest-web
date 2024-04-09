2bbe8393808c36a0ea967913f7e7823d0f08a109
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-04-09 13:44:56 +0300

    fixed person cards

--- src/modules/about-team/person-card.tsx
+++ src/modules/about-team/person-card.tsx
@@ -14,19 +14,21 @@ export const PersonCard = (props: PersonCardProps) => {
     <label className='text-primary'>
       <div className='relative'>
         <img className='w-full h-full' src={props.photo} />
-        <p className='absolute top-6 left-6'>
+        <p className='absolute top-6 left-6 xs:text-base text-xs'>
           Университет
           <br />
-          <span className='text-2xl'>{props.university}</span>
+          <span className='xs:text-2xl text-base'>
+            {props.university}
+          </span>
         </p>
 
-        <p className='absolute top-6 right-6 text-right'>
+        <p className='absolute top-6 right-6 text-right xs:text-base text-xs'>
           Амплуа
           <br />
-          <span className='text-2xl'>{props.amplua}</span>
+          <span className='xs:text-2xl text-base'>{props.amplua}</span>
         </p>
 
-        <p className='absolute bottom-6 left-6 text-4xl'>
+        <p className='absolute bottom-6 left-6 xs:text-4xl text-md'>
           {props.name}
           <br />
           {props.surname}
@@ -35,9 +37,9 @@ export const PersonCard = (props: PersonCardProps) => {
         <a
           href={props.url}
           target='_blank'
-          className='absolute right-10 bottom-10'
+          className='absolute right-5 bottom-5 xs:right-10 xs:bottom-10'
         >
-          <img className='scale-150' src={arrow} />
+          <img className='scale-100 xs:scale-150' src={arrow} />
         </a>
       </div>
     </label>
